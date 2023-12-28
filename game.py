907be06dd3397496f50ee89f3247bca52b8c3d99
Yanis <yanis.crisostomo@proton.me>
2023-12-28 14:46:55 +0100

    28/12/2023

--- game.py
+++ game.py
@@ -11,9 +11,7 @@ curseur = base_donnees.cursor()
 pygame.mixer.init()
 kamehameha = pygame.mixer.Sound("assets/kamehameha-sound-effects-made-with-Voicemod-technology.mp3")
 
-pygame.joystick.init()
-joystick = pygame.joystick.Joystick(0)
-joystick.init()
+
 
 class Jeu:
     def __init__(self):
@@ -23,68 +21,71 @@ class Jeu:
         tmx_data = pytmx.util_pygame.load_pygame("map_assets/map.tmx")
         map_data = pyscroll.TiledMapData(tmx_data)
         map_calques = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        map_calques.zoom = 2
 
-        self.player = Player()
-        self.player.resize(74,108)
+        player_position = tmx_data.get_object_by_name("Joueur")
+        self.player = Player(player_position.x,player_position.y)
+        self.player.resize(49,72,[255,255,255])
 
-        self.group = pyscroll.PyscrollGroup(map_layer=map_calques, default_layer=1)
+        self.group = pyscroll.PyscrollGroup(map_layer=map_calques, default_layer=3)
 
         self.group.add(self.player)
 
+        pygame.joystick.init()
+        self.joystick = pygame.joystick.Joystick(0)
+        self.joystick.init()
 
+    def recup_input_clavier(self):
 
+        pressed = pygame.key.get_pressed()
 
-# class Player_infos():
-#     def __init__(self, screen, font):
-#         self.curseur = curseur
-#         self.joueur_1 = joueur_1
-#         self.joueur_2 = joueur_2
-#         self.rect_p2 = player2_rect
-#         self.screen = screen
-#         self.font = font
-#         self.pv_joueur_1 = 0
-#         self.pv_joueur_2 = 0
-#         self.att_min_joueur_1 = 0
-#         self.att_min_joueur_2 = 0
-#         self.att_max_joueur_1 = 0
-#         self.att_max_joueur_2 = 0
-#
-#     def player_health(self):
-#         self.pv_joueur_1 = \
-#         self.curseur.execute("SELECT PV FROM Stats WHERE NOM LIKE ?", (self.joueur_1,)).fetchone()[0]
-#         self.pv_joueur_2 = \
-#         self.curseur.execute("SELECT PV FROM Stats WHERE NOM LIKE ?", (self.joueur_2,)).fetchone()[0]
-#
-#     def player_attack(self):
-#         self.att_min_joueur_1 = \
-#         self.curseur.execute("SELECT ATT_MIN FROM Stats WHERE NOM LIKE ?", (self.joueur_1,)).fetchone()[0]
-#         self.att_min_joueur_2 = \
-#         self.curseur.execute("SELECT ATT_MIN FROM Stats WHERE NOM LIKE ?", (self.joueur_2,)).fetchone()[0]
-#         self.att_max_joueur_1 = \
-#         self.curseur.execute("SELECT ATT_MAX FROM Stats WHERE NOM LIKE ?", (self.joueur_1,)).fetchone()[0]
-#         self.att_max_joueur_2 = \
-#         self.curseur.execute("SELECT ATT_MAX FROM Stats WHERE NOM LIKE ?", (self.joueur_2,)).fetchone()[0]
-#
-#     def afficher(self, rect_p2):
-#         self.player_health()
-#         texte_surface = self.font.render(f"Pv Joueur 2: {str(self.pv_joueur_2)}", True, (255, 255, 255))
-#         texte_rect = texte_surface.get_rect(midtop=(self.rect_p2.centerx, self.rect_p2.top - 10))
+        if pressed[pygame.K_UP]:
+            print("haut")
+        elif pressed[pygame.K_DOWN]:
+            print("bas")
+        elif pressed[pygame.K_LEFT]:
+            print("gauche")
+        elif pressed[pygame.K_RIGHT]:
+            print("droite")
 
+    def recup_input_manette(self):
 
+        dpad_value = self.joystick.get_hat(0)
+        bouton_A = self.joystick.get_button(0)
+        bouton_B = self.joystick.get_button(1)
+        bouton_X = self.joystick.get_button(2)
+        bouton_Y = self.joystick.get_button(3)
+
+        if dpad_value == (0, 1):
+            print("D-pad vers le haut")
+        elif dpad_value == (0, -1):
+            print("D-pad vers le bas")
+        elif dpad_value == (-1, 0):
+            print("D-pad vers la gauche")
+        elif dpad_value == (1, 0):
+            print("D-pad vers la droite")
+        elif bouton_A:
+            print("bouton A")
+        elif bouton_B:
+            print("bouton B")
+        elif bouton_X:
+            print("bouton X")
+        elif bouton_Y:
+            print("bouton Y")
 
     def run(self):
         running = True
 
-        self.group.draw(self.screen)
-        pygame.display.flip()
-
         while running:
+            self.recup_input_clavier()
+            self.recup_input_manette()
+            self.group.update()
+            self.group.center(self.player.rect.center)
+            self.group.draw(self.screen)
+            pygame.display.flip()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                elif event.type == JOYBUTTONDOWN:
-                    if event.button == 0:
-                        kamehameha.play()
-                        print("Bouton A enfoncé ! Kamehameha!")
 
         pygame.quit()
--- player.py
+++ player.py
@@ -1,14 +1,23 @@
 import pygame
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,x ,y):
         super().__init__()
-        self.sprite = pygame.image.load('assets/sprites/Goku_11.png').convert_alpha()
-        self.image = self.sprite.copy()  # Utilisez une copie pour éviter de modifier l'original
-        self.rect = self.image.get_rect()
+        self.sprite = pygame.image.load('assets/sprites/Goku_11.png')
+        self.rect = self.sprite.get_rect()
+        self.position = [x, y]
 
-    def resize(self, new_width, new_height):
+    def update(self):
+        self.rect.topleft = self.position
+
+    def resize(self, new_width, new_height, colorkey):
+        """
+        :param new_width: int, La nouvelle largeur du sprite voulue.
+        :param new_height: int, La nouvelle hauteur du sprite voulue.
+        :param colorkey: list , une liste de 3 éléments de la forme [255,255,255] (blanc) qui est le code RGB de la couleur à enlever.
+        """
         self.image = pygame.transform.scale(self.sprite, (new_width, new_height))
+        self.image.set_colorkey(colorkey)
         self.rect = self.image.get_rect()
 
 
